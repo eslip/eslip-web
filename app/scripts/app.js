3aade58c2358145d520d82fe91394388fce5f25c
Nicolas Burghi <nicoburghi@gmail.com>
2014-05-29 20:31:20 -0300

    actualizacion de imagen de logo & configuracion de routes para nuevas secciones About y Demo

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,10 +13,17 @@ app.config(function ($routeProvider){
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
     })
-    
+    .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+    })
     .when('/documentation', {
         templateUrl: 'views/documentation.html',
         controller: 'DocumentationCtrl'
+    })
+    .when('/demo', {
+        templateUrl: 'views/demo.html',
+        controller: 'DemoCtrl'
     })
     .when('/support', {
         templateUrl: 'views/support.html',
